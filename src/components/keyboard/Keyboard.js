54c7c00f1cc8f0bd53ee87a7ee94921cfb7572a7
anjumgarg <agarg10_be23@thapar.edu>
2024-08-21 19:53:07 +0530

    first commit

---
+++ src/components/keyboard/Keyboard.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react'
+// import './Keyboard.css';
+const Keyboard = () => 
+    const [inputText, setInputText]= useState('');
+  return (
+    <div>
+      <h1>Hello</h1>
+
+
+      <div className='keyboard'>
+        <div className='textcontainer'>
+            <pre>{inputText}</pre>
+        </div>
+        <>
+    
